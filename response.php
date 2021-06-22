ba33e3234183d4f81a07697fad057d6d96554754
Daiane <daianecalves90@hotmail.com>
2021-06-22 08:39:57 -0300

    Tratando o formulário criando uma página de recebimento mostrando a mensagem enviada

---
+++ response.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href = "style01.css" rel="stylesheet">
+    <link href = "https://www.message-networks.com/wp-content/uploads/2020/01/android_messages_rcs.png" rel='icon'>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;1,100;1,200;1,300;1,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mate+SC&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster+Two&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet">
+    <title>Recebendo sua mensagem</title>
+</head>
+<body>
+    <h1> Muito obrigada pela sua contribuição </h1>
+    <p> Apenas para confirmar, sua mensagem enviada foi: <br> <br> 
+        <span class = "texto"> <?= "$_POST[message]"?> </span> </p>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
